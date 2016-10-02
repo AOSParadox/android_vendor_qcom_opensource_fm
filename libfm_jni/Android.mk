9c4de284162217572893b27b2265fd80ed585430
Louis Popi <theh2o64@gmail.com>
2016-10-02 11:06:58 +0200

    fm: Remove QCOM Flag
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- libfm_jni/Android.mk
+++ libfm_jni/Android.mk
@@ -1,5 +1,5 @@
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
+#ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+#ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -35,6 +35,6 @@ endif
 LOCAL_MODULE := libfmjni
 include $(BUILD_SHARED_LIBRARY)
 
-endif
+#endif
 
-endif # QCOM_HARDWARE
+#endif # QCOM_HARDWARE
